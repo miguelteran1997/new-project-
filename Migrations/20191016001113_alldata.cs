1632c2ebbe20adcc6093e72b61a02f944da0b2e9
miguelteran1997 <54339141+miguelteran1997@users.noreply.github.com>
2019-10-25 14:51:10 -0500

    Add files via upload

---
+++ Migrations/20191016001113_alldata.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OffPractice.Migrations
+{
+    public partial class alldata : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Coach",
+                columns: table => new
+                {
+                    CID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    CN = table.Column<string>(nullable: true),
+                    TID = table.Column<int>(nullable: false),
+                    TN = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Coach", x => x.CID);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Players",
+                columns: table => new
+                {
+                    PID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    PN = table.Column<string>(nullable: true),
+                    PL = table.Column<string>(nullable: true),
+                    CID = table.Column<int>(nullable: false),
+                    TID = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Players", x => x.PID);
+                    table.ForeignKey(
+                        name: "FK_Players_Coach_CID",
+                        column: x => x.CID,
+                        principalTable: "Coach",
+                        principalColumn: "CID",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Players_Team_TID",
+                        column: x => x.TID,
+                        principalTable: "Team",
+                        principalColumn: "TID",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_CID",
+                table: "Players",
+                column: "CID");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Players_TID",
+                table: "Players",
+                column: "TID");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Players");
+
+            migrationBuilder.DropTable(
+                name: "Coach");
+        }
+    }
+}
---
+++ Migrations/20191023194227_migration.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OffPractice.Migrations
+{
+    public partial class migration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "ReleaseDate",
+                table: "Coach",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+
+            migrationBuilder.CreateTable(
+                name: "Bootleg",
+                columns: table => new
+                {
+                    BID = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Date = table.Column<DateTime>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Bootleg", x => x.BID);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Bootleg");
+
+            migrationBuilder.DropColumn(
+                name: "ReleaseDate",
+                table: "Coach");
+        }
+    }
+}
